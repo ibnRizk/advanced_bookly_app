97122d93ec1dbd82c526eabc1c26f0e6891c3515
Hussein Rizk <husseinrizk117@gmail.com>
2025-05-09 18:32:23 +0300

    add either class

--- lib/Features/home/presentation/views/domin/repos/home_repo.dart
+++ lib/Features/home/presentation/views/domin/repos/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:advanced_bookly_app/Features/home/presentation/views/domin/entities/book_entities.dart';
+import 'package:advanced_bookly_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<BookEntities> featchFeaturedBooks();
-  Future<BookEntities> featchNewestBooks();
+  Future<Either<Failure, List<BookEntities>>> featchFeaturedBooks();
+  Future<Either<Failure, List<BookEntities>>> featchNewestBooks();
 }
