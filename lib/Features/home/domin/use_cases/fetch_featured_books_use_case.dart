c73eb1d4e5745f547ddf50f9d7ec2ce82391fc66
Hussein Rizk <husseinrizk117@gmail.com>
2025-05-13 23:25:47 +0300

    impl cubit featch featured books

---
+++ lib/Features/home/domin/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:advanced_bookly_app/Features/home/domin/entities/book_entities.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../../../../core/use_cases/use_case.dart';
+import '../repos/home_repo.dart';
+
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntities>, NoParam> {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntities>>> call([NoParam? param]) async {
+    return await homeRepo.featchFeaturedBooks();
+  }
+}
--- lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:advanced_bookly_app/Features/home/domin/entities/book_entities.dart';
+import 'package:advanced_bookly_app/Features/home/domin/use_cases/fetch_featured_books_use_case.dart';
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+    : super(FeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+  Future<void> featchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+
+    var result = await fetchFeaturedBooksUseCase.call();
+    result.fold(
+      (failure) {
+        emit(FeaturedBooksFailure(failure.errorMessage));
+      },
+      (books) {
+        emit(FeaturedBooksSuccess(books));
+      },
+    );
+  }
 }
